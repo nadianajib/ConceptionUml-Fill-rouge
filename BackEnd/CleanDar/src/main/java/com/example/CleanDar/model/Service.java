e180a812834a9c47bc8ec11a47b24a4990b9e135
nadianajib <nadianajib832@gmail.com>
2024-09-03 22:58:55 +0200

    creer class service et typeservice

---
+++ BackEnd/CleanDar/src/main/java/com/example/CleanDar/model/Service.java
@@ -0,0 +1,24 @@
+package com.example.CleanDar.model;
+
+import jakarta.persistence.*;
+
+
+import lombok.Data;
+
+
+@Data
+@Entity
+public class Service {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nom;
+    private String description;
+    private Double prix;
+
+    @Enumerated(EnumType.STRING)
+    private TypeService typeService;
+}
+
---
+++ BackEnd/CleanDar/src/main/java/com/example/CleanDar/model/TypeService.java
@@ -0,0 +1,8 @@
+package com.example.CleanDar.model;
+
+public enum TypeService {
+    NETTOYAGESTANDARD,
+    CUISINE,
+    SALLE_DE_BAIN
+
+}

29f58bf289ba40405421b6e53dd7ede0cb41f77b
nadianajib <nadianajib832@gmail.com>
2024-09-03 17:29:21 +0100

    creer projet spring

---
+++ BackEnd/CleanDar/src/main/java/com/example/CleanDar/CleanDarApplication.java
@@ -0,0 +1,13 @@
+package com.example.CleanDar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanDarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanDarApplication.class, args);
+	}
+
+}
---
+++ BackEnd/CleanDar/src/test/java/com/example/CleanDar/CleanDarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CleanDar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanDarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

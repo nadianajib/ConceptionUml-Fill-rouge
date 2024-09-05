8f9aa60bc97bfc2d9abb5dc6709213f6f6538730
nadianajib <nadianajib832@gmail.com>
2024-09-05 22:23:19 +0200

    creer package du config

---
+++ BackEnd/CleanDar/src/main/java/com/example/CleanDar/Dao/PersonneRepository.java
@@ -0,0 +1,11 @@
+package com.example.CleanDar.Dao;
+
+import com.example.CleanDar.model.Personne;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PersonneRepository extends JpaRepository<Personne, Long> {
+    Optional<Personne> findByEmail(String email);
+
+}

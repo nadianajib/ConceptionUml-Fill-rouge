0198ae186214563fd26866e510ba53b33af75233
nadianajib <nadianajib832@gmail.com>
2024-09-14 22:47:01 +0200

    creer class personne et user

--- FrontCleanDar/src/app/app.module.ts
+++ FrontCleanDar/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddReservationComponent } from './add-reservation/add-reservation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddReservationComponent
   ],
   imports: [
     BrowserModule,

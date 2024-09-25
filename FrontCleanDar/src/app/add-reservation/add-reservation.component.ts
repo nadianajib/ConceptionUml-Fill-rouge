33c30b91a488ba5c0a9f0c23325ab87115f9c782
nadianajib <nadianajib832@gmail.com>
2024-09-25 17:18:37 +0100

    design login user

--- FrontCleanDar/src/app/add-reservation/add-reservation.component.ts
+++ FrontCleanDar/src/app/add-reservation/add-reservation.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
 import { ReservationService } from '../Services/reservation.service';
-import { Reservation } from '../models/Reservation';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-reservation',
-  templateUrl: './add-reservation.component.html'
+  templateUrl: './add-reservation.component.html',
+  styleUrls: ['./add-reservation.component.scss']
 })
 export class AddReservationComponent implements OnInit {
   reservationForm!: FormGroup;
@@ -21,23 +21,36 @@ export class AddReservationComponent implements OnInit {
     this.reservationForm = this.fb.group({
       dateDebut: ['', Validators.required],
       dateFin: ['', Validators.required],
-      packId: ['', Validators.required] // Utilisez "packId"
+      packId: ['', Validators.required]
     });
+
+    // Ajouter la validation personnalisée pour la vérification des dates
+    this.reservationForm.setValidators(this.dateMismatchValidator);
   }
 
-  onSubmit() {
+  dateMismatchValidator(control: AbstractControl) {
+    const formGroup = control as FormGroup;
+    const dateDebut = formGroup.get('dateDebut')?.value;
+    const dateFin = formGroup.get('dateFin')?.value;
+
+    if (dateDebut && dateFin && new Date(dateDebut) >= new Date(dateFin)) {
+      return { dateMismatch: true };
+    }
+    return null;
+  }
+
+  onSubmit(): void {
     if (this.reservationForm.valid) {
-      const reservationData: Reservation = this.reservationForm.value;
-      console.log(reservationData);
-      this.reservationService.addReservation(reservationData).subscribe({
-        next: (response) => {
+      const reservationData = this.reservationForm.value;
+      this.reservationService.addReservation(reservationData).subscribe(
+        response => {
           console.log('Réservation ajoutée avec succès', response);
-          this.router.navigateByUrl('/dashboard'); 
+          this.router.navigate(['/dashboard']); // Redirige vers la liste des réservations
         },
-        error: (err) => {
-          console.error('Erreur lors de l\'ajout de la réservation', err);
+        error => {
+          console.error('Erreur lors de l\'ajout de la réservation', error);
         }
-      });
+      );
     }
   }
 }
